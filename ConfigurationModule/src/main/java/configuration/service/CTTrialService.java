6c5e86432316b33670b355f1c3df52486d22b4ac
filelife <455587429@qq.com>
2014-08-05 09:32:27 +0800

    后端更新：2014-8-5 09:32:10   试用申请模块界面+dao+service

---
+++ ConfigurationModule/src/main/java/configuration/service/CTTrialService.java
@@ -0,0 +1,33 @@
+package configuration.service;
+
+import configuration.dao.CTTrialDao;
+import model.TTrialRequestModel;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2014/8/5 0005.
+ */
+@Service("configTTrialService")
+public class CTTrialService {
+    @Resource(name="configTTrialDao")
+    CTTrialDao tTrialDao;
+
+    public List<TTrialRequestModel> queryTrialList(){
+        return (List<TTrialRequestModel>)tTrialDao.queryTrialList();
+    }
+
+    public TTrialRequestModel queryOneTrial(String id){
+        return (TTrialRequestModel)tTrialDao.queryOneTrial(id);
+    }
+
+    public void updateTrial(TTrialRequestModel trial){
+        tTrialDao.updateTrial(trial);
+    }
+
+    public void delTrial(TTrialRequestModel trial){
+        tTrialDao.delTrial(trial);
+    }
+}
